b6a0cabebbc29211274fa87a1df4c56db8c4a6fc
LiMingci <mingcich@163.com>
2019-09-01 15:11:17 +0800

    修改CcString的导出宏

--- include/CcString/export.h
+++ include/CcString/export.h
@@ -1,10 +1,14 @@
 #ifndef CC_STRING_EXPORT_H
 #define CC_STRING_EXPORT_H
 
+#ifdef _MSC_VER 
 #ifdef MAKE_CCSTRING_DLL
 #define CCSTR_API __declspec(dllexport)
 #else
 #define CCSTR_API __declspec(dllimport)
+#endif
+#else
+#define CCSTR_API
 #endif
 
 #endif // !CC_STRING_EXPORT_H
